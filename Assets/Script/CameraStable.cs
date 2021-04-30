cb0f057f2dee40674ef84fad0ce0ecd8d3f87baf
Duc Nguyen <ducnrg@gmail.com>
2021-04-30 02:28:22 -0400

    Basic track, finishing line, trigger, laptime UI

--- Assets/Script/CameraStable.cs
+++ Assets/Script/CameraStable.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraStable : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class CameraStable : MonoBehaviour{    
+    public GameObject TheCar;
+    public float CarX, CarY, CarZ;
+
+
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        CarX = TheCar.transform.eulerAngles.x;
+        CarY = TheCar.transform.eulerAngles.y;
+        CarZ = TheCar.transform.eulerAngles.z;
+        // make the camera static
+        transform.eulerAngles = new Vector3(0f,CarY,0f);
     }
 }
